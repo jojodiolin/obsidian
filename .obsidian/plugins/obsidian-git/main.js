9ce0b331ad6e16b10445b249d8347b1f22510a92
Obsidian User <obsidian@local>
2026-06-18 22:38:29 +0800

    初始化 Obsidian 知识库

---
+++ .obsidian/plugins/obsidian-git/main.js
@@ -0,0 +1,9 @@
+/* Obsidian Git Plugin Placeholder
+ *
+ * 此文件是插件占位符。
+ * 请在 Obsidian 中通过社区插件市场安装完整版本：
+ * 1. 打开 Obsidian 设置 → 第三方插件
+ * 2. 关闭"安全模式"
+ * 3. 浏览插件市场，搜索 "obsidian-git"
+ * 4. 点击安装并启用
+ */
